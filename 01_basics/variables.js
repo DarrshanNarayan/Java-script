778748dd3cb7fa7bc4bb50b584454c640d0f9ee2
DarrshanNarayan <darshannarayan17@gmail.com>
2024-11-27 13:00:51 +0000

    commit variables done

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const DOB= 17082000
+let name= "Darshan Narayan"
+let height=6.1
+// let City ;
+var gender="Male";
+let networth="1000cr"
+
+
+console.log(name);
+console.table([DOB, name, height, gender, networth])
+
